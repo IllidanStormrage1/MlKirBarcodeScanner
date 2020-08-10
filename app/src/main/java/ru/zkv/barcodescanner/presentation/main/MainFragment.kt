576a3dc18eac934f837acfab518d2ae7d1f78db5
Kirill <ueurhfyh1@gmail.com>
2020-08-10 16:17:50 +0300

    Add tap to focus

--- app/src/main/java/ru/zkv/barcodescanner/presentation/main/MainFragment.kt
+++ app/src/main/java/ru/zkv/barcodescanner/presentation/main/MainFragment.kt
@@ -1,12 +1,15 @@
 package ru.zkv.barcodescanner.presentation.main
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log.d
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.Camera
+import androidx.camera.core.FocusMeteringAction
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.observe
@@ -15,6 +18,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 import ru.zkv.barcodescanner.R
 import ru.zkv.barcodescanner.databinding.MainFragmentBinding
+import ru.zkv.barcodescanner.domain.model.PreparedCamera
 import ru.zkv.barcodescanner.domain.utils.showToast
 import ru.zkv.barcodescanner.presentation.detail.DetailBottomSheetDialog
 
@@ -22,6 +26,8 @@ class MainFragment : Fragment() {
 
     private val androidViewModel: MainAndroidViewModel by viewModel { parametersOf() }
     private lateinit var binding: MainFragmentBinding
+    private var preparedCamera: PreparedCamera? = null
+    private var camera: Camera? = null
 
     private val permissionCallback =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
@@ -51,6 +57,12 @@ class MainFragment : Fragment() {
 
         permissionCallback.launch(Manifest.permission.CAMERA)
 
+        configureCamera()
+
+        binding.mainBottomBar.setNavigationOnClickListener {
+            pickImageCallback.launch("image/*")
+        }
+
         androidViewModel.barcodeLiveData.observe(viewLifecycleOwner) {
             when (it) {
                 is UIState.Success -> {
@@ -66,23 +78,35 @@ class MainFragment : Fragment() {
                 }
             }
         }
+    }
 
+    @SuppressLint("ClickableViewAccessibility")
+    private fun configureCamera() {
         androidViewModel.cameraProviderLiveData.observe(viewLifecycleOwner) {
             it.run {
                 processCameraProvider.unbindAll()
-                processCameraProvider.bindToLifecycle(
+                camera = processCameraProvider.bindToLifecycle(
                     this@MainFragment,
                     selector,
                     preview,
                     analyzer
                 )
                 preview.setSurfaceProvider(binding.mainCamera.createSurfaceProvider())
+                preparedCamera = it
             }
         }
 
-        binding.mainBottomBar.setNavigationOnClickListener {
-            pickImageCallback.launch("image/*")
+        binding.mainCamera.setOnTouchListener { view, motionEvent ->
+            preparedCamera?.let {
+                if (motionEvent.action != MotionEvent.ACTION_UP) return@let
+                val factory = binding.mainCamera.createMeteringPointFactory(it.selector)
+                val point = factory.createPoint(motionEvent.x, motionEvent.y)
+                val action = FocusMeteringAction.Builder(point).build()
+                camera?.cameraControl?.startFocusAndMetering(action)
+            }
+            return@setOnTouchListener true
         }
+
     }
 
     companion object {
